c5e737ca5399c2c8f7b24f61cda8e3883fe5091a
tianyuningmou <tianyuningmou2009@126.com>
2018-03-28 19:21:14 +0800

    word2vec txt

---
+++ word2vec.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright () 2018
+
+All rights reserved
+
+FILE: word2vec.py
+AUTHOR: tianyuningmou
+DATE CREATED:  @Time : 2018/3/28 下午7:12
+
+DESCRIPTION:  .
+
+VERSION: : #1 
+CHANGED By: : tianyuningmou
+CHANGE:  : 
+MODIFIED: : @Time : 2018/3/28 下午7:12
+"""
+
+
