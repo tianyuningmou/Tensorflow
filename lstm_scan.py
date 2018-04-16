1a973f777d4a081d1586a0bfa69fae408085d4f6
tianyuningmou <tianyuningmou2009@126.com>
2018-04-16 17:17:57 +0800

    lstm_scan

---
+++ lstm_scan.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright () 2018
+
+All rights reserved
+
+FILE: lstm_scan.py
+AUTHOR: tianyuningmou
+DATE CREATED:  @Time : 2018/4/16 下午5:15
+
+DESCRIPTION:  .
+
+VERSION: : #1 
+CHANGED By: : tianyuningmou
+CHANGE:  : 
+MODIFIED: : @Time : 2018/4/16 下午5:15
+"""
+
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+
+
+class LSTM_Cell(object):
+
