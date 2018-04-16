ec1b2f691845a5d23ea169ecdac063237ce2c553
tianyuningmou <tianyuningmou2009@126.com>
2018-04-16 19:05:04 +0800

    lstm_scan

--- lstm_scan.py
+++ lstm_scan.py
@@ -25,3 +25,21 @@ import time
 
 class LSTM_Cell(object):
 
+    def __init__(self, incoming, D_input, D_cell, initializer, f_bias=1.0):
+        self.incoming = incoming
+        self.D_input = D_input
+        self.D_cell = D_cell
+
+        self.W_xi = initializer([self.D_input, self.D_cell])
+        self.W_hi = initializer([self.D_cell, self.D_cell])
+        self.b_i = tf.Variable(tf.zeros([self.D_cell]))
+
+        self.W_xf = initializer([self.D_input, self.D_cell])
+        self.W_hf = initializer([self.D_cell, self.D_cell])
+        self.b_f = tf.Variable(tf.constant(f_bias, shape=[self.D_cell]))
+
+        self.W_xo = initializer([self.D_input, self.D_cell])
+        self.W_ho = initializer([self.D_cell, self.D_cell])
+        self.b_o = tf.Variable(tf.zeros([self.D_cell]))
+
+        pass
