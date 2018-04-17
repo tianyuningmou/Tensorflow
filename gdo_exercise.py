3642df1841f704f07d15fda5f2b60c9c3b35953c
tianyuningmou <tianyuningmou2009@126.com>
2018-04-17 13:17:48 +0800

    gdo_exercise

--- gdo_exercise.py
+++ gdo_exercise.py
@@ -23,6 +23,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+# 第一部分
 def add_layer(inputs, in_size, out_size, activation_function=None):
     # 添加层并返回层的输出结果
     Weights = tf.Variable(tf.random_normal([in_size, out_size]))
@@ -82,3 +83,33 @@ for i in range(1000):
        # plot the prediction
        lines = ax.plot(x_data, prediction_value, 'r-', lw=5)  # 'r-'指绘制一个红色的线
        plt.pause(0.5)  # 指等待一秒钟
+
+
+# 第二部分
+# Model parameters
+W = tf.Variable([.3], dtype=tf.float32)
+b = tf.Variable([-.3], dtype=tf.float32)
+# Model input and output
+x = tf.placeholder(tf.float32)
+linear_model = W*x + b
+y = tf.placeholder(tf.float32)
+
+# loss
+loss = tf.reduce_sum(tf.square(linear_model - y)) # sum of the squares
+# optimizer
+optimizer = tf.train.GradientDescentOptimizer(0.01)
+train = optimizer.minimize(loss)
+
+# training data
+x_train = [1, 2, 3, 4]
+y_train = [0, -1, -2, -3]
+# training loop
+init = tf.global_variables_initializer()
+sess = tf.Session()
+sess.run(init) # reset values to wrong
+for i in range(1000):
+  sess.run(train, {x: x_train, y: y_train})
+
+# evaluate training accuracy
+curr_W, curr_b, curr_loss = sess.run([W, b, loss], {x: x_train, y: y_train})
+print("W: %s b: %s loss: %s"%(curr_W, curr_b, curr_loss))
