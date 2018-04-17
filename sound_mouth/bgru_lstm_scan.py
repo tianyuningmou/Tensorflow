d4d0f87b40613e165522ba3fca058609d37d0297
tianyuningmou <tianyuningmou2009@126.com>
2018-04-17 13:09:12 +0800

    bgru_lstm_scan

---
+++ sound_mouth/bgru_lstm_scan.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright () 2018
+
+All rights reserved
+
+FILE: bgru_lstm_scan.py
+AUTHOR: tianyuningmou
+DATE CREATED:  @Time : 2018/4/17 下午1:08
+
+DESCRIPTION:  .
+
+VERSION: : #1 
+CHANGED By: : tianyuningmou
+CHANGE:  : 
+MODIFIED: : @Time : 2018/4/17 下午1:08
+"""
+
+
