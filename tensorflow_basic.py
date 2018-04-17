3c97b03e448c5a04eb380c4b93cd6d666ca090df
tianyuningmou <tianyuningmou2009@126.com>
2018-04-17 13:13:37 +0800

    tensorflow_basic

--- tensorflow_basic.py
+++ tensorflow_basic.py
@@ -28,3 +28,22 @@ x.initializer.run()
 
 sub = tf.subtract(x, a)
 print(sub.eval())
+
+
+# 创建一个变量，初始化为标量0
+state = tf.Variable(0, name='counter')
+
+# 创建一个OP，起作用是使state增加1
+one = tf.constant(1)
+new_value = tf.add(state, one)
+update = tf.assign(state, new_value)
+
+# 启动图后，必须先经过'初始化'op
+init_op = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+    sess.run(init_op)
+    print(sess.run(state))
+    for _ in range(3):
+        sess.run(update)
+        print(sess.run(state))
